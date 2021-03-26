e1fbe8edb10b7bffeb2ffe0b791f2de10a57da8e
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-03-26 13:32:26 +0100

    Flere design overvejelser, mere sjov i main

--- PantryBEtest/Models/Item.cs
+++ PantryBEtest/Models/Item.cs
@@ -3,24 +3,19 @@ using DataBase.Models;
 
 namespace DataBase
 {
-    abstract public class IItem
-    {
-        abstract public int ItemId { get; set; }
-        //public List<string> EanCollection { get; set; }//Flere varenumre kan være en del af et Item fx. Kærgåden og Bakkedal smørbar
-        abstract public string Name { get; set; }
-
-    }
-    public class Item:IItem
+  
+    public class Item
     {
         //public int ItemId { get; set; }
-        public override int ItemId { get; set; }
+        public  int ItemId { get; set; }
 
         //public List<string> EanCollection { get; set; }//Flere varenumre kan være en del af et Item fx. Kærgåden og Bakkedal smørbar
-        public override string Name { get; set; }
+        public  string Name { get; set; }
         public ICollection<InventoryItem> InventoryCollection { get; set; } //En vare kan eksistere på flere lister (Fx smør i fryser og køleskab)
 
-        public int amountInfridge { get; set; }
-        public int amountInFreezer { get; set; }
+        public uint AverageLifespanDays { get; set; }
+        public uint Size { get; set; }//Mass, Volume or amount
+        public uint DesiredMinimumAmount { get; set; } //Nødvendig for at genere indkøbslite
 
     }
 }
