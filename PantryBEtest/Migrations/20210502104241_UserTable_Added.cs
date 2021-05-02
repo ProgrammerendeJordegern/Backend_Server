6a3234b83a2e7800ebdba94545f20f45815a7fee
AnnSo <64138186+auAnnSo@users.noreply.github.com>
2021-05-02 12:43:40 +0200

    Updated Database wth new Userdb and table UserDbo. Added Migration and controller.

---
+++ PantryBEtest/Migrations/20210502104241_UserTable_Added.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataBase.Migrations
+{
+    public partial class UserTable_Added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(96)", maxLength: 96, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(254)", maxLength: 254, nullable: true),
+                    PwHash = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ PantryBEtest/Models/UserDb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataBase.Models
+{
+    public class UserDb
+    {
+        [Key] 
+        public long UserId { get; set; }
+        [MaxLength(96)] 
+        public string FullName { get; set; }
+
+        [MaxLength(254)] 
+        public string Email { get; set; }
+
+        [MaxLength(64)] 
+        public string PwHash { get; set; }
+
+
+
+
+    }
+}
