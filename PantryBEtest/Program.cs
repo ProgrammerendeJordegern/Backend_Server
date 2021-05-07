4915f5829e4fc7844aa7841e65cecb3e700637f6
AnnSo <64138186+auAnnSo@users.noreply.github.com>
2021-05-07 12:36:07 +0200

    tuborg

--- PantryBEtest/Program.cs
+++ PantryBEtest/Program.cs
@@ -40,72 +40,72 @@ namespace DataBase
                     {
                        Console.WriteLine( elem.Item.Name);
                     }
-                    
-
-        //            Console.WriteLine("Getting inventories:");
-        //            var inventories = UOW.Users.GetInventoriesWithUser(1);
-        //            if (inventories != null) { 
-        //                var fridge = inventories.SingleOrDefault(i => i.GetType() == typeof(Fridge));
-        //                Console.WriteLine(inventories.ElementAt(1).GetType());
-        //                //Print content in fridge
-        //                Console.WriteLine("Indhold i køleskab: ");
-        //                foreach (var item in fridge.ItemCollection)
-        //                {
-        //                    Console.WriteLine(item.Item.Name+ ": "+ item.Amount);
-        //                }
-        //            }
-                    
-
-        //        ////Search for user and change name
-        //        //Console.WriteLine("Indtast bruger ID:");
-        //        //    var input = Console.ReadLine();
-        //        //    PpUser user = UOW.Users.Get(int.Parse(input));
-        //        //    if (user != null) { 
-        //        //    Console.WriteLine("Name: "+user.Name+" Type new Name: ");
-        //        //    input = Console.ReadLine();
-        //        //    user.Name = input;
-        //        //    UOW.Complete();
-        //        //    }
-        //        //    else Console.WriteLine("Could not find user.");
-
-        //        //    //Get passwordHash
-        //        //    PpUser u2 = UOW.Users.GetUserWithEmail("mail@mail.dk");
-        //        //    Console.WriteLine(u2.PasswordHash);
-        //        }
-
-               
 
 
-        //    }
-        //}
+                //            Console.WriteLine("Getting inventories:");
+                //            var inventories = UOW.Users.GetInventoriesWithUser(1);
+                //            if (inventories != null) { 
+                //                var fridge = inventories.SingleOrDefault(i => i.GetType() == typeof(Fridge));
+                //                Console.WriteLine(inventories.ElementAt(1).GetType());
+                //                //Print content in fridge
+                //                Console.WriteLine("Indhold i køleskab: ");
+                //                foreach (var item in fridge.ItemCollection)
+                //                {
+                //                    Console.WriteLine(item.Item.Name+ ": "+ item.Amount);
+                //                }
+                //            }
 
-        //public static void AddDummyData(MyDbContext dbc)
-        //    {
-        //        using (dbc )
-        //        {
-        //            var i1 = new Item() {Name = "Smør", InventoryCollection = new List<InventoryItem>(),Ean = "123123"};
-        //            var i2 = new Item() {Name = "Mælk", InventoryCollection = new List<InventoryItem>()};
-        //            var i3 = new Item() {Name = "Æg", InventoryCollection = new List<InventoryItem>()};
-        //            var i4 = new Item() {Name = "Toiletpapir", InventoryCollection = new List<InventoryItem>()};
 
-        //            var inventory1 = new Fridge() { ItemCollection = new List<InventoryItem>()};
-        //            var inventory2 = new Freezer() { ItemCollection = new List<InventoryItem>()};
-        //            var inventory3 = new Pantry() { ItemCollection = new List<InventoryItem>()};
+                //        ////Search for user and change name
+                //        //Console.WriteLine("Indtast bruger ID:");
+                //        //    var input = Console.ReadLine();
+                //        //    PpUser user = UOW.Users.Get(int.Parse(input));
+                //        //    if (user != null) { 
+                //        //    Console.WriteLine("Name: "+user.Name+" Type new Name: ");
+                //        //    input = Console.ReadLine();
+                //        //    user.Name = input;
+                //        //    UOW.Complete();
+                //        //    }
+                //        //    else Console.WriteLine("Could not find user.");
 
-        //            var ii1 = new InventoryItem() {Amount = 2, Inventory = inventory1, Item = i1};
-                    
-        //            var ii2 = new InventoryItem() { Amount = 3, Inventory = inventory1, Item = i2 };
-                    
-        //            var ii3 = new InventoryItem() { Amount = 1, Inventory = inventory1, Item = i3 };
-                    
-        //            var ii4 = new InventoryItem() { Amount = 8, Inventory = inventory3, Item = i4 };
+                //        //    //Get passwordHash
+                //        //    PpUser u2 = UOW.Users.GetUserWithEmail("mail@mail.dk");
+                //        //    Console.WriteLine(u2.PasswordHash);
+                //        }
 
-        //            dbc.InventoryItem.Add(ii4);
-        //            dbc.InventoryItem.Add(ii1);
-        //            dbc.InventoryItem.Add(ii2);
-        //            dbc.InventoryItem.Add(ii3);
 
-                
+
+
+                //    }
+                //}
+
+                //public static void AddDummyData(MyDbContext dbc)
+                //    {
+                //        using (dbc )
+                //        {
+                //            var i1 = new Item() {Name = "Smør", InventoryCollection = new List<InventoryItem>(),Ean = "123123"};
+                //            var i2 = new Item() {Name = "Mælk", InventoryCollection = new List<InventoryItem>()};
+                //            var i3 = new Item() {Name = "Æg", InventoryCollection = new List<InventoryItem>()};
+                //            var i4 = new Item() {Name = "Toiletpapir", InventoryCollection = new List<InventoryItem>()};
+
+                //            var inventory1 = new Fridge() { ItemCollection = new List<InventoryItem>()};
+                //            var inventory2 = new Freezer() { ItemCollection = new List<InventoryItem>()};
+                //            var inventory3 = new Pantry() { ItemCollection = new List<InventoryItem>()};
+
+                //            var ii1 = new InventoryItem() {Amount = 2, Inventory = inventory1, Item = i1};
+
+                //            var ii2 = new InventoryItem() { Amount = 3, Inventory = inventory1, Item = i2 };
+
+                //            var ii3 = new InventoryItem() { Amount = 1, Inventory = inventory1, Item = i3 };
+
+                //            var ii4 = new InventoryItem() { Amount = 8, Inventory = inventory3, Item = i4 };
+
+                //            dbc.InventoryItem.Add(ii4);
+                //            dbc.InventoryItem.Add(ii1);
+                //            dbc.InventoryItem.Add(ii2);
+                //            dbc.InventoryItem.Add(ii3);
+
+
 
                 //PpUser u1 = new PpUser()
                 //    {
@@ -114,7 +114,7 @@ namespace DataBase
                 //        Name = "Børge",
                 //        PasswordHash = "123"
                 //    };
-                
+
 
                 //u1.Inventories.Add(inventory1);
                 //    u1.Inventories.Add(inventory2);
@@ -142,12 +142,12 @@ namespace DataBase
                 //    dbc.PpUser.Add(u2);
 
                 //    dbc.SaveChanges();
-                //}
-
             }
-        
-        
 
-        
+        }
+
+
+
+
     }
 }
